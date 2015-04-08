d1a72341ade8d06aaf9a4d02a6ff47c028e1847b
Tom Peerdeman <tom@tompeerdeman.nl>
2015-04-08 21:45:01 +0200

    Finalized REST

--- Assign1/REST/index.php
+++ Assign1/REST/index.php
@@ -2,84 +2,6 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-
-/* DEEL LAURENS */
-// if ($db = new SQLite3('urls.db')) {
-//     $q = $db->query('CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY NOT NULL, url TEXT NOT NULL)');
-    
-//     switch ($_SERVER['REQUEST_METHOD']) {
-//         case "GET":
-//             $q = "SELECT * FROM urls";
-//             if (isset($_GET['id'])) {
-//                 $q.= " WHERE id=\"" . $_GET['id'] . "\"";
-//             }
-//             $r = $db->query($q);
-//             if ($r) {
-//                 while ($entry = $r->fetchArray()) {
-//                     if (isset($_GET['id'])) {
-//                         header("HTTP/1.1 301 Moved");
-//                         header("Location: " . $entry['url']);
-//                         die();
-//                     }
-//                     echo $entry['id'] . ",";
-//                 }
-//             }
-//             if (isset($_GET['id'])) {
-//                 header("HTTP/1.1 404");
-//             }
-//             break;
-
-//         case "POST":
-//             if (isset($_POST['url']) && filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
-//                 $r = $db->query("SELECT id FROM urls WHERE url=\"" . $_POST['url'] . "\"");
-//                 $row = $r->fetchArray();
-//                 if (!$row) {
-//                     if (!$db->query("INSERT INTO urls (url) VALUES(\"" . $_POST['url'] . "\")")) {
-//                         die($db->lastErrorMsg());
-//                     }
-//                     $r = $db->query("SELECT id FROM urls WHERE url=\"" . $_POST['url'] . "\"");
-//                     $row = $r->fetchArray();
-//                     if (!$row) {
-//                         die($db->lastErrorMsg());
-//                     }
-//                 }
-//                 echo $row['id'];
-//             } else {
-//             	header("HTTP/1.1 400 Provide valid url");
-//             }
-//             break;
-//         /* Not tested yet..*/
-//         case "DELETE":
-//         	/* PHP only parses $_GET and $_POST automatically, we have to parse $_DELETE manually */
-//         	parse_str(file_get_contents('php://input'), $_DELETE);
-//             $q = "DELETE FROM urls";
-//             if (isset($_GET['id'])) {
-//                 $q.= " WHERE id=\"" . $_GET['id'] . "\"";
-//             }
-//             $r = $db->query($q);
-//             if($r || !isset($_GET['id'])){
-//             	header("HTTP/1.1 200");
-//             } else {
-//         		header("HTTP/1.1 404");
-//             }
-//             break;
-//         /* Not tested yet..*/
-//         case "PUT":
-//             /* PHP only builds $_GET and $_POST automatically, we have to build $_PUT manually */
-//             parse_str(file_get_contents('php://input'), $_PUT);
-//             if(isset($_GET['id']) && isset($_PUT['url']) && filter_var($_PUT['url'], FILTER_VALIDATE_URL)){
-//             	$q = "UPDATE urls SET url=\"" . $_PUT['url'] . "\" WHERE id=\"" . $_GET['id'] . "\"";
-// 	            $r = $db->query($q);
-// 	            if($r){
-// 	            	header("HTTP/1.1 204");
-// 	            } else {
-// 	        		header("HTTP/1.1 404");
-// 	            }
-//             } else {
-//             	header("HTTP/1.1 400 Provide valid url and id");
-//             }
-//             break;
-
 if($db = new SQLite3('urls.db')) {
 	$db->query('CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY NOT NULL, url TEXT NOT NULL)');
 
@@ -170,14 +92,11 @@ if($db = new SQLite3('urls.db')) {
 			$row = $r->fetchArray();
 			// Record with the given id exists.
 			if($row) {
-				// PHP is stupid and doesn't parse PUT requests as POST, therefore 
-				// we have to parse the data ourselves.
 				if(isset($_PUT['url']) && filter_var($_PUT['url'], FILTER_VALIDATE_URL)) {
-				
-						if(!$db->query("UPDATE urls SET url=\"" . $_PUT['url'] . "\" WHERE id=" . intval($_GET['id']))) {
-							header("HTTP/1.1 500");
-							die($db->lastErrorMsg());
-						}
+					if(!$db->query("UPDATE urls SET url=\"" . $_PUT['url'] . "\" WHERE id=" . intval($_GET['id']))) {
+						header("HTTP/1.1 500");
+						die($db->lastErrorMsg());
+					}
 				} else {
 					header("HTTP/1.1 400 Invalid URL");
 					echo "Invalid URL";
@@ -187,9 +106,6 @@ if($db = new SQLite3('urls.db')) {
 			}
 
 			break;
-	}
-
-        default:
     }
 } else {
     die($db->lastErrorMsg());
