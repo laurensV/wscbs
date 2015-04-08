5d6e277306b7e5b15deab35895a4068c51fa9570
TomPeerdeman <tom@tompeerdeman.nl>
2015-04-08 16:35:53 +0200

    Working SQLITE

--- Assign1/REST/index.php
+++ Assign1/REST/index.php
@@ -2,8 +2,8 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-if($db = new SQLiteDatabase('filename')) {
-	$q = @$db->query('CREATE TABLE IF NOT EXISTS urls (id TEXT, url TEXT, PRIMARY KEY (id))';
+if($db = new SQLite3('urls.db')) {
+	$q = $db->query('CREATE TABLE IF NOT EXISTS urls (id INT AUTO_INCREMENT, url VARCHAR(255), PRIMARY KEY (id))');
 
 	switch($_SERVER['REQUEST_METHOD']) {
 		case "GET":
@@ -13,22 +13,30 @@ if($db = new SQLiteDatabase('filename')) {
 			}
 			$r = $db->query($q);
 			if($r) {
-				while ($entry = sqlite_fetch_array($r, SQLITE_ASSOC)) {
+				while ($entry = $r->fetchArray()) {
 					if(isset($_GET['id'])) {
 						header("HTTP/1.1 301 Moved");
 						header("Location: " . $entry['url']);
 						die();
 					}
-					var_dump($entry);
+					echo $entry['id'] .  "\n";
 				}
+			}
+			if(isset($_GET['id'])) {
+				header("HTTP/1.1 404");
 			}
 			break;
 		case "POST":
 			if(isset($_POST['url']) && filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
-				$id = 9001;
-				if(!$db->query("INSERT INTO urls (id, url) VALUES(\"" . $id . "\", \"" . $_POST['url'] . "\")")) {
-					die($db->sqlite_last_error());
+				if(!$db->query("INSERT INTO urls (url) VALUES(\"" . $_POST['url'] . "\")")) {
+					die($db->lastErrorMsg());
 				}
+				$r = $db->query("SELECT id FROM urls WHERE url=\"" . $_POST['url'] . "\"");
+				$row = $r->fetchArray();
+				if(!$row) {
+					die($db->lastErrorMsg());
+				}
+				echo $row['id'];
 			}
 			break;
 		case "DELETE":
@@ -41,6 +49,6 @@ if($db = new SQLiteDatabase('filename')) {
 	}
 
 } else {
-	die($db->sqlite_last_error());
+	die($db->lastErrorMsg());
 }
 ?>
