735bc06d0a65c9302c6649831509922543f811bd
Laurens Verspeek <laurens_verspeek@hotmail.com>
2015-04-08 21:20:09 +0200

    added PUT and DELETE request handling for REST service

---
+++ Assign1/REST/client.php
@@ -0,0 +1,15 @@
+<form action="index.php" method="post">
+URL: <input type="text" name="url"  /><br />
+<input type="submit" name="submit" value="Shorten URL" />
+</form>
+<?php
+/******* GENERATE PUT AND DELETE REQUEST **********/
+// $query_string = "custom=query_strings";
+// $ch = curl_init('http://requestdomain.com');
+// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); // Or DELETE
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: ' . strlen($query_string)));
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $query_string);
+// echo curl_exec($ch);
+// curl_close($ch);
+?>
--- Assign1/REST/index.php
+++ Assign1/REST/index.php
@@ -2,57 +2,85 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-if($db = new SQLite3('urls.db')) {
-	$q = $db->query('CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY NOT NULL, url TEXT NOT NULL)');
+if ($db = new SQLite3('urls.db')) {
+    $q = $db->query('CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY NOT NULL, url TEXT NOT NULL)');
+    
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case "GET":
+            $q = "SELECT * FROM urls";
+            if (isset($_GET['id'])) {
+                $q.= " WHERE id=\"" . $_GET['id'] . "\"";
+            }
+            $r = $db->query($q);
+            if ($r) {
+                while ($entry = $r->fetchArray()) {
+                    if (isset($_GET['id'])) {
+                        header("HTTP/1.1 301 Moved");
+                        header("Location: " . $entry['url']);
+                        die();
+                    }
+                    echo $entry['id'] . ",";
+                }
+            }
+            if (isset($_GET['id'])) {
+                header("HTTP/1.1 404");
+            }
+            break;
 
-	switch($_SERVER['REQUEST_METHOD']) {
-		case "GET":
-			$q = "SELECT * FROM urls";
-			if(isset($_GET['id'])) {
-				$q .= " WHERE id=\"" . $_GET['id'] . "\"";
-			}
-			$r = $db->query($q);
-			if($r) {
-				while ($entry = $r->fetchArray()) {
-					if(isset($_GET['id'])) {
-						header("HTTP/1.1 301 Moved");
-						header("Location: " . $entry['url']);
-						die();
-					}
-					echo $entry['id'] .  ",";
-				}
-			}
-			if(isset($_GET['id'])) {
-				header("HTTP/1.1 404");
-			}
-			break;
-		case "POST":
-			if(isset($_POST['url']) && filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
-				$r = $db->query("SELECT id FROM urls WHERE url=\"" . $_POST['url'] . "\"");
-				$row = $r->fetchArray();
-				if(!$row) {
-					if(!$db->query("INSERT INTO urls (url) VALUES(\"" . $_POST['url'] . "\")")) {
-						die($db->lastErrorMsg());
-					}
-					$r = $db->query("SELECT id FROM urls WHERE url=\"" . $_POST['url'] . "\"");
-					$row = $r->fetchArray();
-					if(!$row) {
-						die($db->lastErrorMsg());
-					}
-				}
-				echo $row['id'];
-			}
-			break;
-		case "DELETE":
+        case "POST":
+            if (isset($_POST['url']) && filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
+                $r = $db->query("SELECT id FROM urls WHERE url=\"" . $_POST['url'] . "\"");
+                $row = $r->fetchArray();
+                if (!$row) {
+                    if (!$db->query("INSERT INTO urls (url) VALUES(\"" . $_POST['url'] . "\")")) {
+                        die($db->lastErrorMsg());
+                    }
+                    $r = $db->query("SELECT id FROM urls WHERE url=\"" . $_POST['url'] . "\"");
+                    $row = $r->fetchArray();
+                    if (!$row) {
+                        die($db->lastErrorMsg());
+                    }
+                }
+                echo $row['id'];
+            } else {
+            	header("HTTP/1.1 400 Provide valid url");
+            }
+            break;
+        /* Not tested yet..*/
+        case "DELETE":
+        	/* PHP only parses $_GET and $_POST automatically, we have to parse $_DELETE manually */
+        	parse_str(file_get_contents('php://input'), $_DELETE);
+            $q = "DELETE FROM urls";
+            if (isset($_DELETE['id'])) {
+                $q.= " WHERE id=\"" . $_DELETE['id'] . "\"";
+            }
+            $r = $db->query($q);
+            if($r || !isset($_DELETE['id'])){
+            	header("HTTP/1.1 200");
+            } else {
+        		header("HTTP/1.1 404");
+            }
+            break;
+        /* Not tested yet..*/
+        case "PUT":
+            /* PHP only builds $_GET and $_POST automatically, we have to build $_PUT manually */
+            parse_str(file_get_contents('php://input'), $_PUT);
+            if(isset($_PUT['id']) && isset($_PUT['url']) && filter_var($_PUT['url'], FILTER_VALIDATE_URL)){
+            	$q = "UPDATE urls SET url=\"" . $_PUT['url'] . "\" WHERE id=\"" . $_PUT['id'] . "\"";
+	            $r = $db->query($q);
+	            if($r){
+	            	header("HTTP/1.1 204");
+	            } else {
+	        		header("HTTP/1.1 404");
+	            }
+            } else {
+            	header("HTTP/1.1 400 Provide valid url and id");
+            }
+            break;
 
-			break;
-		case "PUT":
-
-			break;
-		default:
-	}
-
+        default:
+    }
 } else {
-	die($db->lastErrorMsg());
+    die($db->lastErrorMsg());
 }
 ?>
