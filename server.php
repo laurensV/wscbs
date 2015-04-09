584656ea67a6c93e635350e00c255e02e00f01c7
Laurens Verspeek <laurens_verspeek@hotmail.com>
2015-04-09 12:50:18 +0200

    better structure + generated Client

--- server.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
-require_once('WSDL/class.phpwsdl.php');
-require_once('calculator.class.php');
-
-PhpWsdl::RunQuickMode('calculator.class.php');
-?>
