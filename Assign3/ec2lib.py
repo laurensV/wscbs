23228b451eb968e8370a570a5bba2f77827c524b
Tom Peerdeman <tom@tompeerdeman.nl>
2015-05-05 16:53:29 +0200

    Control EC2 instances from code

---
+++ Assign3/ec2lib.py
@@ -0,0 +1,80 @@
+import boto
+import boto.ec2
+import base64
+
+from threading import Timer
+from time import time
+from ec2pricing import *
+
+class EC2Lib:
+	#base64.b64encode(
+	RUN_CODE = """#!/bin/bash -xe
+	sh /home/ubuntu/start.sh
+	"""
+
+	conn = None
+	instances = []
+	prices = {}
+
+	def __init__(self, region, access_key, secret):
+		self.conn = boto.ec2.connect_to_region(region, aws_access_key_id=access_key, aws_secret_access_key=secret)
+		self.prices = get_ec2_pricing(region)
+
+	def start_instance(self, ami, instance_type):
+		reservation = self.conn.run_instances(image_id = ami,
+                                     key_name='group07_2',
+                                     instance_type=instance_type,
+                                     security_group_ids=['sg-b72273d3'],
+                                     subnet_id='subnet-85003ead',
+                                     instance_initiated_shutdown_behavior='terminate',
+                                     user_data=self.RUN_CODE)
+
+		
+		for instance in reservation.instances:
+			instance.add_tag('InstanceOwner', 'ccengstud07')
+			self.instances.append(EC2Instance(instance, prices[instance_type]))
+
+		return reservation
+
+	def get_security_groups(self):
+		return self.conn.get_all_security_groups();
+
+	def get_key_pairs(self):
+		return self.conn.get_all_key_pairs();
+
+	def stop_all(self):
+		for instance in self.instances:
+			instance.stop(0)
+		
+
+class EC2Instance:
+	start_time = 0
+	stop_scheduled = False
+	instance = None
+	price = 0.0
+
+	def __init__(self, instance, price):
+		self.instance = instance
+		self.price = price
+		self.start_time = time()
+
+	def run_time(self):
+		return time() - self.start_time;
+
+	def run_cost(self):
+		return (self.run_time() / 3600) * self.price
+
+	def stop(self, wait_time):
+		if(self.instance != None):
+			print "Schedule termination of %s after %d seconds" % (self.instance.id, wait_time)
+			stop_scheduled = True
+			Timer(wait_time, self.terminate_self).start()
+
+	def terminate_self(self):
+		if(self.instance != None):
+			print "Terminating %s" % self.instance.id
+			self.instance.stop(True)
+			self.instance.terminate()
+			self.instance = None
+	
+
---
+++ Assign3/ec2pricing.py
@@ -0,0 +1,24 @@
+import re, string, urllib2, ujson
+
+def get_ec2_pricing(region_code):
+	url = 'http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js'
+	response = urllib2.urlopen(url);
+	respdata = response.read()
+	idx = string.find(respdata, 'callback(');
+
+	table = {}
+
+	# Strip javascript.
+	respdata = respdata[(idx + 9):(len(respdata) - 2)]
+	# Transform lazy JSON into proper JSON, that we can read.
+	# {key: value} --> {"key": value}
+	respdata = re.sub(r"(\w+):", r'"\1":', respdata)
+	data = ujson.loads(respdata)
+	for region in data["config"]["regions"]:
+		if(region["region"] == region_code):
+			for gen in region["instanceTypes"]:
+				for instance in gen["sizes"]:
+					table[instance["size"]] = float(instance["valueColumns"][0]["prices"]["USD"])
+
+	return table
+
